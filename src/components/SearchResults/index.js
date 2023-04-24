822428acd5e4993e635e8190026f8732e2bf214f
Carter Brainerd <0xCB@protonmail.com>
2023-04-24 00:38:40 -0400

    Search improvements

--- src/components/SearchResults/index.js
+++ src/components/SearchResults/index.js
@@ -36,9 +36,11 @@ const SearchResults = ({ searchQuery }) => {
         </div>
         <br></br>
         <h1 className="text-3xl font-bold text-center">External Articles</h1>
-        {externalArticles.map((article) => (
-          <Link to={article.url}><ArticleCard key={article.id} article={article} /></Link>
-        ))}
+        <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-4 ps-4">
+          {externalArticles.map((article) => (
+            <Link to={article.url}><ArticleCard key={article.id} article={article} /></Link>
+          ))}
+        </div>
       </>
     )
   }
