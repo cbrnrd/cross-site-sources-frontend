a7522050dc6c1adb800ec96ef135294f2ab97faa
Carter Brainerd <0xCB@protonmail.com>
2023-04-23 22:58:24 -0400

    Finally get ViewArticlePage working

--- src/App.js
+++ src/App.js
@@ -25,9 +25,6 @@ const store = configureStore({
 
 
 function App() {
-  // Check if `jwt` is in localStorage
-  // If it is, set `isLoggedIn` to true
-  // If it is not, set `isLoggedIn` to false
   return (
     <Provider store={store}>
       <BrowserRouter>
